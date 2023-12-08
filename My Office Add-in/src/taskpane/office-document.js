a149c8f8c7dfb24fac4bbadbe167ba9be4f66196
Xodzi <43913874+Xodzi@users.noreply.github.com>
2023-12-09 01:10:36 +0300

    Dialog API and Formula get

--- My Office Add-in/src/taskpane/office-document.js
+++ My Office Add-in/src/taskpane/office-document.js
@@ -1,11 +1,25 @@
 /* global Excel console */
 
+
+
 const insertText = async (text) => {
   // Write text to the top left cell.
   try {
     Excel.run(async (context) => {
+      let dialog; // Declare dialog as global for use in later functions.
+      Office.context.ui.displayDialogAsync('https://localhost:3000/taskpane.html?dialogID=15', {height: 30, width: 20},
+          function (asyncResult) {
+              dialog = asyncResult.value;
+              dialog.addEventHandler(Office.EventType.DialogMessageReceived, processMessage);
+          }
+      )
       const sheet = context.workbook.worksheets.getActiveWorksheet();
-      const range = sheet.getRange("A1");
+      let range = context.workbook.getSelectedRange();
+      range.load("formulas");
+      await context.sync();
+      console.log(range.formulas);
+      //sheetPropertiesChanged(range.formulas);
+      //const range = sheet.getRange("A1");
       range.values = [[text]];
       range.format.autofitColumns();
       return context.sync();
@@ -14,5 +28,6 @@ const insertText = async (text) => {
     console.log("Error: " + error);
   }
 };
+
 
 export default insertText;
