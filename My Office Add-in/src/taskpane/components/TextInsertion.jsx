be8d9522a05c165eb531f70062273a0c84ff6b27
Xodzi <43913874+Xodzi@users.noreply.github.com>
2023-12-29 14:50:39 +0300

    Finally tree generate auto

--- My Office Add-in/src/taskpane/components/TextInsertion.jsx
+++ My Office Add-in/src/taskpane/components/TextInsertion.jsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import { Button, Field, Textarea, tokens, makeStyles } from "@fluentui/react-components";
 import insertText from "../office-document";
 import ArrayComponent from "../components/ArrayComponent";
@@ -26,46 +26,22 @@ const useStyles = makeStyles({
 
 //-------------------------------------------------------------------
 // add localStorage get into testArray
-const testArray = [
-  {
-    name: 'SUM(MAX(3,5),AVERAGE(1,2,3,4,SUM(7,7)),ABS(SUM(-7,4,1)))',
-    depth: 0,
-    res: '43',
-  },
-  {
-    name: 'MAX(3,5)',
-    depth: 1,
-    res: '4',
-  },
-  {
-    name: 'AVERAGE(1,2,3,4,SUM(7,7))',
-    depth: 1,
-    res: '4',
-  },
-  {
-    name: 'SUM(7,7)',
-    depth: 2,
-    res: '4',
-  },
-  {
-    name: 'ABS(SUM(-7,4,1))',
-    depth: 1,
-    res: '1',
-  },
-  {
-    name: 'SUM(-7,4,1)',
-    depth: 2,
-    res: '1',
-  }
-];
 //-------------------------------------------------------------------
 
 
 const TextInsertion = () => {
   const [text, setText] = useState("Some text.");
 
-  const handleTextInsertion = async () => {
-    console.log( await insertText());
+  const [array,SetArray] = useState([])
+
+
+  async function handleTextInsertion() {
+    let test = await insertText();
+    console.log(test);
+    const treeData = JSON.parse(localStorage.getItem('arrayData'));
+    console.log(treeData)
+    console.log("SetArray")
+    SetArray(treeData);
   };
 
   const handleTextChange = async (event) => {
@@ -74,13 +50,14 @@ const TextInsertion = () => {
 
   const styles = useStyles();
 
+
   return (
     <div className={styles.textPromptAndInsertion}>
       <Field className={styles.instructions}>Click the button to create formula tree.</Field>
       <Button appearance="primary" disabled={false} size="large" onClick={handleTextInsertion}>
         Create tree
       </Button>
-      <ArrayComponent valuesFormulaArray={testArray}></ArrayComponent>
+      <ArrayComponent valuesFormulaArray={array}></ArrayComponent>
     </div>
   );
 };
--- My Office Add-in/src/taskpane/office-document.js
+++ My Office Add-in/src/taskpane/office-document.js
@@ -36,20 +36,36 @@ function createCellName(row, column) {
 
 function getSubFormulas(node) {
   if (node.type === "function") {
-    var name = `${node.name}(${node.arguments.map(getSubFormulas).join(",")})`;
-    // console.log(formula)
+    var args = node.arguments.map(arg => {
+   //   console.log(arg)
+      if (arg.arguments) {
+        // Если аргумент - это массив, извлекаем свойство name из каждого элемента
+     //   console.log(arg.arguments.map(subArg => getSubFormulas(subArg).name).join(","))
+        return arg.name +"(" +arg.arguments.map(subArg => getSubFormulas(subArg).name).join(",") + ")";
+      } else {
+        // Если аргумент не является массивом, извлекаем свойство name
+        return getSubFormulas(arg).name;
+      }
+    }).join(",");
+    
+    var name = `${node.name}(${args})`;
+   // console.log(name);
     const formula = {
       name: name,
-      depth: 0 //node.depth
+      depth: node.depth
     };
     return [formula, ...node.arguments.filter((elem) => elem.type == "function").map(getSubFormulas).flat()];
   } else {
-    if(node.operand == null){
-      return node.value
+    if (node.operand == null) {
+      let temp_name = node.value;
+      return {name: temp_name}
     }
-    return 0-node.operand.value;
+    let temp_name = 0 - node.operand.value;
+    return {name : temp_name}
   }
 }
+
+
 
 //------------------------------------------------
 
@@ -58,23 +74,12 @@ function getSubFormulas(node) {
 //------------------------------------------------
 
 
-
-
-
-
-
-function traverseTree(node, depth = 0) {
-        
-  if (node.arguments != undefined && node.arguments.length > 0) {
-    console.log(`${"  ".repeat(depth)}${node.name} - Depth: ${depth}`);
-    node.arguments.forEach((childNode) => {
-      console.log(node, depth)
-      traverseTree(childNode, depth + 1);
-
-    });
-  }
-  else{
-    console.log(node);
+function setDepth(node, depth) {
+  if (node.type === "function" && node.arguments.length > 0) {
+    node.depth = depth;
+    node.arguments.forEach((arg) => setDepth(arg, depth + 1));
+  } else {
+    node.depth = depth;
   }
 }
 
@@ -92,9 +97,8 @@ function traverseTree(node, depth = 0) {
 // main function
 //------------------------------------------------
 const insertText = async () => {
-  
   try {
-    Excel.run(async (context) => {
+    await Excel.run(async (context) => {
       
       let range = context.workbook.getSelectedRange();
       range.load("formulas");
@@ -125,22 +129,21 @@ const insertText = async () => {
 
 
       //________________________________________________ create arrays with split formulas (сначала надо довести до ума функцию, которая сплитует нашу строку)
-      //const lettersFormulaArray = parse(lettersFormula);
-      //const valuesFormulaArray = parse(valuesFormula);
-      //console.log(lettersFormulaArray);
-      //console.log(valuesFormulaArray);
+
       console.log(valuesFormula);
       const valuesPIZDEZ = parse(valuesFormula);
+
+      setDepth(valuesPIZDEZ,0);
       //________________________________________________
 
 
       //________________________________________________ create array with pieces formulas and calculate their values
       // example of ideal formula split (здесь должна использоваться именно valuesFormulaArray, потому что по ней ведутся дальнейшие вычисления и создание formulasValuesMap)
       //var valuesFormulaArray = ["SUM(SUM(1,2),ABS(4),3,AVERAGE(MAX(8,1,5),SUM(4,3,7)))", "SUM(1,2)", "ABS(4)", "3", "AVERAGE(MAX(8,1,5),SUM(4,3,7))", "MAX(8,1,5)", "SUM(4,3,7)"];
-      console.log(valuesPIZDEZ)
       var valuesFormulaArray = getSubFormulas(valuesPIZDEZ);
-      console.log(valuesFormulaArray);
+
       context.workbook.worksheets.getItemOrNullObject("SpecialCalculationField").delete(); // delete old calculation field
+
       const creatFieldSheet = context.workbook.worksheets.add("SpecialCalculationField"); // add new calculation field
 
       const formulasObjectsArray = [];
@@ -149,30 +152,32 @@ const insertText = async () => {
 
       var formulasValuesMap = new Map();
 
-      valuesFormulaArray[0] = valuesFormula.substring(1, valuesFormula.length);
+      valuesFormulaArray[0] = {
+        name : valuesFormula.substring(1, valuesFormula.length),
+        depth : 0
+      };
 
       for (var i=0; i<valuesFormulaArray.length; i++) {
         const calcSheet = context.workbook.worksheets.getItem("SpecialCalculationField");
         let calcRange = calcSheet.getRange("A1");
-        calcRange.formulas = [["=" + valuesFormulaArray[i]]];
+        calcRange.formulas = [["=" + valuesFormulaArray[i].name]];
         calcRange = calcSheet.getRange("A1");
         calcRange.load("text");
         await context.sync();
         const formulaObject = {
-          name: valuesFormulaArray[i], // valuesFormulaArray[i].name
-          depth: cur_d+1, // valuesFormulaArray[i].depth // Здесь должно быть значение глубины, но оно пока не известно
+          name: valuesFormulaArray[i].name, // valuesFormulaArray[i].name
+          depth: valuesFormulaArray[i].depth, // valuesFormulaArray[i].depth // Здесь должно быть значение глубины, но оно пока не известно
           res: calcRange.text[0][0]
         };
         formulasObjectsArray.push(formulaObject);
 
     
-        //console.log(calcRange.text[0][0]); 
         formulasValuesMap.set(valuesFormulaArray[i], calcRange.text[0][0]);
       }
-      console.log(formulasValuesMap)
-      console.log(formulasObjectsArray)
 
-      //console.log([...formulasValuesMap.entries()]);
+      var jsonString = JSON.stringify(formulasObjectsArray);
+      await localStorage.setItem('arrayData', JSON.stringify(formulasObjectsArray));
+      console.log("обновили")
 
       context.workbook.worksheets.getItemOrNullObject("SpecialCalculationField").delete(); // delete new calculation field
       await context.sync();
@@ -182,7 +187,7 @@ const insertText = async () => {
       //________________________________________________ declare dialog as global for use in later functions.
       //return(formulasValuesMap);
       let dialog;
-      Office.context.ui.displayDialogAsync('https://localhost:3000/taskpane.html?dialogID=15&lettersFormula=' + lettersFormula + '&valuesFormula=' + valuesFormula + '&tree=' + tree, {height: 30, width: 20},
+      Office.context.ui.displayDialogAsync('https://localhost:3000/taskpane.html?dialogID=15&lettersFormula=' + lettersFormula + '&valuesFormula=' + valuesFormula, {height: 30, width: 20},
           function (asyncResult) {
               dialog = asyncResult.value;
               dialog.addEventHandler(Office.EventType.DialogMessageReceived, processMessage);
@@ -193,6 +198,7 @@ const insertText = async () => {
   } catch (error) {
     console.log("Error: " + error);
   }
+  return "Залупа"
 };
 //------------------------------------------------
 
